d92c614bb85221fbd1d71e258f2f3386072da832
samhalo007 <samhalo007@hotmail.ca>
2014-02-15 14:08:24 -0500

    Sources de mon mod en 1.7.2 !
    Sources de mon mod en 1.7.2 !

---
+++ src/main/java/fr/mrsamhalo/samhaloworldmod/SamHaloWorldMod.java
@@ -0,0 +1,19 @@
+package fr.mrsamhalo.samhaloworldmod;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SamHaloWorldMod.MODID, version = SamHaloWorldMod.VERSION)
+public class SamHaloWorldMod
+{
+    public static final String MODID = "samhaloworld";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+}
